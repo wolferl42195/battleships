401b970411cc4d619d3ec226ef3ec00673c41f71
wolferl42195 <wolfgang.radinger@gmail.com>
2019-11-24 12:59:11 +0100

    ships classes refecored

--- src/main/java/sample/Ship.java
+++ src/main/java/sample/Ship.java
@@ -2,14 +2,22 @@ package sample;
 
 import java.util.ArrayList;
 
-public class Ship
-{
-    private ArrayList<ShipPart> shipparts = new ArrayList<>();
+public class Ship {
+
+    private ArrayList<ShipPart> shipParts = new ArrayList<>();
+
     private int length;
+
     private int x;
+
     private int y;
+
     private Direction direction;
-    private int divx, divy;
+
+    private int divx;
+
+    private int divy;
+
 
     public int getX()
     {
@@ -43,17 +51,12 @@ public class Ship
 
     public ArrayList<ShipPart> getShipParts()
     {
-        return shipparts;
+        return shipParts;
     }
 
-    /*ist das unterste Level vom Schiff erstellen. Wird in der Ship Methode aufgerufen. Dort bekommt es die
-    Übergabeparameter und hier können dann die ShipParts zur ArrayList hinzugefügt werden. Kette geht bis zu main
-    Methode saveShips hinauf.*/
-    private void generateShip(int x, int y, int length, Direction directions)
-    {
-        for (int i = 0; i < length; i++)
-        {
-            shipparts.add(new ShipPart(x, y));
+    private void generateShip(int x, int y, int length, Direction directions) {
+        for (int i = 0; i < length; i++) {
+            shipParts.add(new ShipPart(x, y));
             switch (directions)
             {
                 case UP:
@@ -72,39 +75,23 @@ public class Ship
                     y++;
                     break;
             }
-
         }
     }
 
-    /*Kriegt die Koordinaten von setShip (Field Klasse), welche die Koordinaten von saveShip(main) übergeben bekommt.
-     Also man muss mehrere Methoden "überstehen", um wirklich hier zu landen. Es wird dazwischen überprüft ob man
-     setzen darf, ob Platz frei ist usw. Am Rande: Vergleichbar mit GUI am PC. Man macht was und es geht viele
-     Schichten hinunter. */
-    public Ship(int x, int y, int length, Direction directions, int diffvectorx, int diffvectory)
-    {
+    public Ship(int x, int y, int length, Direction directions, int diffVectorX, int diffVectorY) {
         this.x = x;
         this.y = y;
         this.direction = directions;
         this.length = length;
-        this.divx = diffvectorx;
-        this.divy = diffvectory;
+        this.divx = diffVectorX;
+        this.divy = diffVectorY;
 
         generateShip(x, y, length, directions);
-
-        /*Dient nur der Ausgabe für uns zum testen*/
-        System.out.println("ich generiere schiff an X= " + this.x + " Y =" + this.y + " richtung" + this.direction + " länge =" + this.length);
     }
 
-    /*Die Schleife geht jeden part vom Schiff durch. Die if Bedienung checkt für jeden part (das macht es bei jedem
-    Schleifendurchgang) ob die x und y Koordinate vom Part, das gerade in der For Schleife durchlaufen wird, den
-    übergebenen x und y Koordinaten entspricht. (Die übergebenen, werden in der attack Methode in Field übergeben).
-    Wenn es zutrifft, setzt es damage von dem Part auf true (das macht die destroy Methode in ShipPart)*/
-    public boolean attack(int x, int y)
-    {
-        for (ShipPart shippart : this.shipparts)
-        {
-            if (shippart.getX() == x && shippart.getY() == y)
-            {
+    public boolean attack(int x, int y) {
+        for (ShipPart shippart : this.shipParts) {
+            if (shippart.getX() == x && shippart.getY() == y) {
                 shippart.destroy();
                 return true;
             }
@@ -112,16 +99,9 @@ public class Ship
         return false;
     }
 
-/*Das macht im Prinzip das gleiche wie die attack Methode. Nur diesmal schaut es, ob das ganze Schiff zerstört ist. Wie?
- Es prüft, für jeden Part (mit der For Schleife), ob es damaged ist. Wenn nicht damaged (das macht das Rufzeichen
- vorm ShipPart in der If - Bedienung) für einen part, false. Wenn für alle parts true zurückgeliefert wird, kommt bei
-  der Methode true raus. Sprich, Schiff ist zerstört.*/
-    public boolean checkIfDestroyed()
-    {
-        for (ShipPart shippart : this.shipparts)
-        {
-            if (!shippart.isDamaged())
-            {
+    public boolean checkIfDestroyed() {
+        for (ShipPart shippart : this.shipParts) {
+            if (!shippart.isDamaged()) {
                 return false;
             }
         }
